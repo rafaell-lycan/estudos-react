24227a28fde0e2f43a16d416e411f4b5d2dea4c9
Rafaell Lycan <sonny.webdsg@gmail.com>
2015-10-20 21:07:41 -0200

    Add basic packages and gulp to build tasks

---
+++ react-browserify/gulpfile.js
@@ -0,0 +1,35 @@
+var gulp = require('gulp'),
+    browserify = require('gulp-browserify'),
+    concat = require('gulp-concat'),
+    browserSync = require('browser-sync').create();
+
+gulp.task('browserify', ['copy'], function(){
+  gulp.src('src/main.jsx')
+  .pipe(browserify({
+    transform: 'reactify',
+    debug: true
+  }))
+  .pipe(concat('app.js'))
+  .pipe(gulp.dest('dist/js'))
+  .pipe(browserSync.stream());
+});
+
+gulp.task('copy', function(){
+  gulp.src('src/index.html')
+  .pipe(gulp.dest('dist'))
+  .pipe(browserSync.stream());
+});
+
+gulp.task('browsersync', function(){
+  browserSync.init({
+    server: {
+      baseDir: "./dist"
+    }
+  });
+});
+
+gulp.task('watch', ['browsersync'], function(){
+  gulp.watch('src/**/*.*', ['default']);
+});
+
+gulp.task('default', ['browserify']);
