b8f106d6fcc228064117da3c0a2f0ab72c9fcccb
Rafaell Lycan <sonny.webdsg@gmail.com>
2015-10-25 16:22:25 -0200

    Add ESLint support

--- mini-player-webpack/webpack.config.js
+++ mini-player-webpack/webpack.config.js
@@ -1,40 +1,40 @@
 module.exports = {
-  context: __dirname + "/app",
+  context: __dirname + '/app',
 
   entry: {
-    javascript: "./js/app.js",
-    html: "./index.html"
+    javascript: './js/app.js',
+    html: './index.html'
   },
 
   output: {
-    filename: "app.js",
-    path: __dirname + "/dist",
+    filename: 'app.js',
+    path: __dirname + '/dist',
     publicPath: '/'
   },
 
-  devtool: "source-map",
+  devtool: 'source-map',
 
   module: {
     loaders: [
       {
         test: /.jsx?$/,
         exclude: /(node_modules|bower_components)/,
-        loaders: ["react-hot", "babel"],
+        loaders: ['react-hot', 'babel']
       },
       {
         test: /\.html$/,
-        loaders: ["file?name=[name].[ext]"],
+        loaders: ['file?name=[name].[ext]']
       },
       {
         test: /\.scss$/,
         exclude: /(node_modules|bower_components)/,
-        loaders: ["style", "css", "sass"]
+        loaders: ['style', 'css', 'sass']
       },
       {
         test: /\.json$/,
         exclude: /(node_modules|bower_components)/,
-        loaders: ["file?name=[name].[ext]", "json"]
-      },
-    ],
-  },
+        loaders: ['file?name=[name].[ext]', 'json']
+      }
+    ]
+  }
 }
