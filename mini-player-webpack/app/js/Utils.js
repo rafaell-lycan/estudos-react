5cd4b411781c540d9b0460bd094b3ec544eec429
Rafaell Lycan <sonny.webdsg@gmail.com>
2015-10-24 23:01:28 -0200

    Add Utils

---
+++ mini-player-webpack/app/js/Utils.js
@@ -0,0 +1,11 @@
+let Utils = {
+  formatBgUrl : (url) => `url(${url})`,
+  formatTimeDuration: (duration) => {
+    if(typeof duration === "string") return duration;
+
+    let time = (duration / 1000 /60);
+    return time.toFixed(2).replace('.',':');
+  }
+}
+
+export default Utils;
